dbe290ed39d5ea8876c732ca263a3f392d3ff76e
Leo724-dev <leleo456nardo@gmail.com>
2026-04-19 18:03:50 -0300

    Modificações no HTML e no CSS e finalização do JS.

--- script.js
+++ script.js
@@ -1,8 +1,49 @@
 const mensagem = document.querySelector(".mensagem");
 const imagem = document.querySelector("#imagem");
+const titulo = document.querySelector("h1");
+const rodape = document.querySelector(".rodape");
 
 function horarioDia(){
-    alert("Tudo Funcionando");
+    const agora = new Date();
+    const hora = agora.getHours();
+    
+    if(hora < 6){
+        mensagem.innerHTML = `Agora são exatamente ${hora} horas`;
+
+        imagem.src = "img/madrugada.jpg";
+
+        document.body.style.backgroundColor = "#C5B9A5";
+
+        titulo.style.color = "#3E3A32"
+        rodape.style.color = "#6D6558";
+    }else if(hora < 12){
+        mensagem.innerHTML = `Agora são exatamente ${hora} horas`;
+
+        imagem.src = "img/manha.jpg";
+
+        document.body.style.backgroundColor = "#A3D977";
+
+        titulo.style.color = "#6a9639"
+        rodape.style.color = "#3D522B";
+    }else if(hora < 18){
+         mensagem.innerHTML = `Agora são exatamente ${hora} horas`;
+
+        imagem.src = "img/tarde.jpg";
+
+        document.body.style.backgroundColor = "#D98345";
+
+        titulo.style.color = "#2B1605"
+        rodape.style.color = "#5E300B";
+    }else{
+        mensagem.innerHTML = `Agora são exatamente ${hora} horas`;
+
+        imagem.src = "img/noite.jpg";
+
+        document.body.style.backgroundColor = "#1A242B";
+        
+        titulo.style.color = "#E0E6ED"
+        rodape.style.color = "#8A9FB1";
+    }
 }
 
 window.addEventListener("load", horarioDia);
