3bb1f3aa18750c5cb604aada80b7be3e07e58009
Leo724-dev <leleo456nardo@gmail.com>
2026-04-19 18:27:32 -0300

    Fiz o Media Querie do projeto e adcionei os minutos no JS.

--- script.js
+++ script.js
@@ -6,9 +6,10 @@ const rodape = document.querySelector(".rodape");
 function horarioDia(){
     const agora = new Date();
     const hora = agora.getHours();
+    const minutos = agora.getMinutes();
     
     if(hora < 6){
-        mensagem.innerHTML = `Agora são exatamente ${hora} horas`;
+        mensagem.innerHTML = `Agora são exatamente ${hora}:${minutos}`;
 
         imagem.src = "img/madrugada.jpg";
 
@@ -17,7 +18,7 @@ function horarioDia(){
         titulo.style.color = "#3E3A32"
         rodape.style.color = "#6D6558";
     }else if(hora < 12){
-        mensagem.innerHTML = `Agora são exatamente ${hora} horas`;
+        mensagem.innerHTML = `Agora são exatamente ${hora}:${minutos}`;
 
         imagem.src = "img/manha.jpg";
 
@@ -26,7 +27,7 @@ function horarioDia(){
         titulo.style.color = "#6a9639"
         rodape.style.color = "#3D522B";
     }else if(hora < 18){
-         mensagem.innerHTML = `Agora são exatamente ${hora} horas`;
+         mensagem.innerHTML = `Agora são exatamente ${hora}:${minutos}`;
 
         imagem.src = "img/tarde.jpg";
 
@@ -35,12 +36,12 @@ function horarioDia(){
         titulo.style.color = "#2B1605"
         rodape.style.color = "#5E300B";
     }else{
-        mensagem.innerHTML = `Agora são exatamente ${hora} horas`;
+        mensagem.innerHTML = `Agora são exatamente ${hora}:${minutos}`;
 
         imagem.src = "img/noite.jpg";
 
         document.body.style.backgroundColor = "#1A242B";
-        
+
         titulo.style.color = "#E0E6ED"
         rodape.style.color = "#8A9FB1";
     }
