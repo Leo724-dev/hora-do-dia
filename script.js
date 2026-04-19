bed1a97ec225df3bcf4293db427704bda687f39c
Leo724-dev <leleo456nardo@gmail.com>
2026-04-19 17:17:00 -0300

    Criação da página e inicío do script.

---
+++ script.js
@@ -0,0 +1,8 @@
+const mensagem = document.querySelector(".mensagem");
+const imagem = document.querySelector("#imagem");
+
+function horarioDia(){
+    alert("Tudo Funcionando");
+}
+
+window.addEventListener("load", horarioDia);
